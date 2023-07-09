8356ad19f435a7742c31b1edee813c6b82e7a00c
Paulo Victor <paulovmel@gmail.com>
2023-07-09 16:30:17 -0300

    feat: creates product details

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: productdetail</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+import { ProductDetails } from "./components/product-details";
+
+const App = () => <ProductDetails />;
 ReactDOM.render(<App />, document.getElementById("app"));
